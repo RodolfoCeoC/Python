7c88c6b18818e768603d1bfc84d5e32b2a7dcc6c
Rodolfo ceo cortes <ceosmarck@gmail.com>
2022-04-27 01:03:24 -0500

    sixth

--- facturas.py
+++ facturas.py
@@ -2,6 +2,7 @@
 # dict.pop('Nombre') #pop permite borrar algun elemento del diccionario, para ellos colocamos dentro del parentesis la clave de referencia,
 # print(di
 dic_facturas = {}
+pago_total = []
 bienvenidos = """
 -----------------------------------------
    Bienvenidos a la gestión de facturas   
@@ -20,27 +21,47 @@ while seguir:
     print(bienvenidos)
     selec = input("Ingrese un numero: ")
     if selec == "1":
-        factura = input("Ingrese un numero de factura: ")
-        cantidad = float(input("Ingrese una cantidad de pesos Mexicanos a su factura: "))
-        dic_facturas[factura] = cantidad
-        print("La factura numero " + factura,"con la cantidad de " + str(cantidad),"pesos se guardó con éxito")
-        seguir = input("¿Deseas continuar?: ") == "si"
+        while seguir:
+            factura = input("Ingrese un numero de factura: ")
+            if factura not in dic_facturas.keys():
+                cantidad = float(input("Ingrese una cantidad de pesos Mexicanos a su factura: "))
+                dic_facturas[factura] = cantidad
+                print("La factura numero " + factura, "con la cantidad de " + str(cantidad),
+                      "pesos se guardó con éxito")
+                seguir = input("¿Deseas continuar?: ") == "si"
+                break
+            else:
+                print("¡La factura ya existe!")
+                seguir = input("¿Deseas volver a intentarlo?: ") == "si"
 
     elif selec == "2":
         if len(dic_facturas) == 0:
             print("No hay facturas disponibles, ingrese una nueva")
             seguir = input("¿Desea regresar al menú principal?: ") == "si"
         elif len(dic_facturas) != 0:
-            print(dic_facturas)
+            numeros = [str(j) for j in dic_facturas.keys()]
+            numero_2 = [str(k) for k in dic_facturas.values()]
+            elementos = 0
+            while elementos < len(dic_facturas):
+                print("Factura",numeros[elementos],"\t", numero_2[elementos],"pesos","\n--------------------------")
+                elementos += 1
+                break
             pagar = input("Escribe el número de la factura que deseas pagar: ")
             if pagar not in dic_facturas.keys():
                 print("No se encuentra el elemento")
             else:
-                print("Seguro quiere pagar la factura " + pagar)
-                eliminar = dic_facturas.pop(pagar)
-                print("¡La factura se pagó con exito!")
-                seguir = input("¿Desea continuar?: ") == "si"
+                check = input("Seguro quiere pagar la factura " + pagar +" :")
+                if check == "si":
+                    pago = dic_facturas.get(pagar)
+                    pago_total.append(pago)
+                    eliminar = dic_facturas.pop(pagar)
+                    print("¡La factura se pagó con exito!")
+                    print("El total pagado hasta ahora es de " + str(sum(pago_total)),"pesos","y la cantidad restante es de "
+                          + str(sum(dic_facturas.values())),"pesos")
+                    seguir = input("¿Desea realizar otra operación?: ") == "si"
+                else:
+                    seguir = input("¿Deseas regresar al menu principal?: ") == "si"
 
     else:
-        print("_______________________________","\n¡La opción ingresada no existe!")
+        print("_______________________________", "\n¡La opción ingresada no existe!")
         seguir = input("¿Quieres volver a intentarlo?: ") == "si"
