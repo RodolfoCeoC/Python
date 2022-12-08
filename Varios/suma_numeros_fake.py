14b68c0af93d30a546efbe2336b19c01264dac20
Rodolfo Ceo Cortes <ceosmarck@gmail.com>
2022-12-08 13:38:37 -0600

    troll

---
+++ Varios/suma_numeros_fake.py
@@ -0,0 +1,9 @@
+from barra_carga import barra as carga
+
+print("Bienvenido a la suma de números")
+num_1 = input("Ingresa el primer número: ")
+num_2 = input("Ingresa el segundo número: ")
+
+print("\n" +"Aplicando métodos avanzados de inteligencia artificial...")
+carga()
+print("\n" +"El resultado de la suma es:","\n" + num_1 + num_2)
