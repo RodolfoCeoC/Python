205f9c9a0374989bddd2fac4da1d26c1ee1bba74
Rodolfo Ceo Cortes <ceosmarck@gmail.com>
2022-12-07 22:57:53 -0600

    v_1

--- Varios/barra_carga.py
+++ Varios/barra_carga.py
@@ -1,5 +1,9 @@
 import time
 
-for i in range(1, 51):
-    print("[" + i * "#" + "]" + str(i * 2) + "%", end="\r")
-    time.sleep(0.05)
+def barra():
+    for i in range(1, 51):
+        print("[" + i * "#" + "]" + str(i * 2) + "%", end="\r")
+        time.sleep(0.06)
+        
+if __name__ == "__main__":
+    barra()
