eecdaa51344296f6e934200581ab0253e771e31c
Rodolfo Ceo Cortes <ceosmarck@gmail.com>
2022-12-08 12:29:53 -0600

    second

--- Funciones/Factorial.py
+++ Funciones/Factorial.py
@@ -4,7 +4,7 @@ def factorial():
     temp = 1                 #Creamos una variable temporal que empezara desde el 1
     for i in range(numero):  #Usamos un for en donde la variable i va a iterar los numeros segun el rango establecido
         temp *= i+1          #La variable temporal va iniciar desde el 1 multiplicando todos los números hasta el rango de i
-    return temp              #Vamos a retornar la variable temp para poderla invocar en el print
+        result = "El factorial de: {} es: {}".format(numero, temp) #Usamos un format para almacenar el número ingresado y el factorial de ese número
+    return result
 
-print(factorial())           #Mandamos a invocar la función, como argumento no le ponemos nada porque el input está
-                             #Dentro de la variable.
+print(factorial())
