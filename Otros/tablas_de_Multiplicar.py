17bfe9fb2bb49003a11fd551fee2c70e546f2d6f
Rodolfo Ceo Cortes <ceosmarck@gmail.com>
2022-12-13 11:20:43 -0600

    second

--- Otros/tablas_de_Multiplicar.py
+++ Otros/tablas_de_Multiplicar.py
@@ -2,7 +2,9 @@
 # Creamos un for con los numeros principales que son del 1 al 10
 # Luego creamos otro for dentro del for en donde los numeros seran por los que se van a multiplicar
 
+print("Tablas de multiplicar","\n","-"*156,end="\n")
+
 for i in range(1, 11):
     for j in range(1, 11):
-        print(str(j) + " X " + str(i) + " = " + str(i*j), end="\t")
-    print("")
+        print("|",str(j) + " X " + str(i) + " = " + str(j * i),end="\t")
+    print()
